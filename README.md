3c911252e669f73e4168bca7f71fcbf1b5460b3d
ThaDewey <thadewey@gmail.com>
2020-02-12 22:36:03 -0600

    d

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 - [How to Make a Discord Bot: an Overview and Tutorial](https://www.toptal.com/chatbot/how-to-make-a-discord-bot)
 - [Discord API Reference](https://discordapp.com/developers/docs/reference)
 - [application Informaiton](https://discordapp.com/developers/applications/471730407537377316/oauth)
-- [Eris GitHub](https://github.com/abalabahaha/eris)
+- [discord.js GitHub](https://discord.js.org/#/)
+- [heroku.com](heroku.com)
---
+++ index.js
@@ -0,0 +1,53 @@
+const Discord = require('discord.js');
+
+const bot = new Discord.Client();
+
+const token = 'Njc3MzQ0MDc4MTU3MDUzOTYy.XkS4DA.u0VN_bGOY8jtv2cD_bXWJicBQuk';
+
+const PREFix = '!';
+
+bot.on('ready', () => {
+	console.log('Bot is online');
+});
+
+bot.on('message', msg => {
+	let args = msg.content.substring(PREFix.length).split(" ");
+
+
+	switch (args[0]) {
+		case 'ping':
+			msg.reply("!!PONG!!!");
+			break;
+		case 'websites':
+			msg.channel.send('link to website');
+			break;
+		case 'info':
+			if (args[1] === 'version') {
+
+				msg.channel.send('version 1.1.1');
+			} else {
+
+				msg.channel.send('shut up');
+			}
+
+			break;
+		case 'embed':
+			const embed = new Discord.RichEmbed().addField('Player Name', msg.author.username);
+			msg.channel.sendEmbed(embed);
+			break;
+		default:
+			msg.channel.send('Like, sorry. I dont know about that.');
+			break;
+	}
+
+
+	if (msg.content === "HELLO") {
+		msg.reply('Hello Friend');
+
+	}
+
+
+});
+
+
+bot.login(token);
